d0ba6a4b30e9879255781ff23ef514a0e299c189
VMrGamer <v.mr.gamer@gmail.com>
2019-02-06 10:49:08 +0530

    Bla

--- app/src/main/java/vmrgamer/firebasecloudmessaging/MainActivity.java
+++ app/src/main/java/vmrgamer/firebasecloudmessaging/MainActivity.java
@@ -4,18 +4,24 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initializeFirebaseAuthListner();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String channelId  = getString(R.string.default_notification_channel_id);
             String channelName = getString(R.string.default_notification_channel_name);
@@ -31,5 +37,20 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Key: " + key + " Value: " + value);
             }
         }
+    }
+
+    private void initializeFirebaseAuthListner() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG, ": User signed in: " + user.getUid());
+                } else {
+                    Log.d(TAG, "User signed out");
+                }
+            }
+        };
     }
 }
