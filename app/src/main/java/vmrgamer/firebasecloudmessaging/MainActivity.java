0ae57b7be0d79caa021229aa5766fc0da2c903a9
VMrGamer <v.mr.gamer@gmail.com>
2019-02-08 15:29:52 +0530

    Notification Channel Moved to Separate Class

--- app/src/main/java/vmrgamer/firebasecloudmessaging/MainActivity.java
+++ app/src/main/java/vmrgamer/firebasecloudmessaging/MainActivity.java
@@ -2,20 +2,33 @@ package vmrgamer.firebasecloudmessaging;
 
 //kamehameha
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.functions.FirebaseFunctions;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private EditText mFirstNumberField;
+    private EditText mSecondNumberField;
+    private EditText mAddResultField;
+    private EditText mMessageInputField;
+    private EditText mMessageOutputField;
+    private Button mCalculateButton;
+    private Button mAddMessageButton;
+    private Button mSignInButton;
+
+    private FirebaseFunctions mFunctions;
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListner;
 
@@ -24,21 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeFirebaseAuthListner();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId  = getString(R.string.default_notification_channel_id);
-            String channelName = getString(R.string.default_notification_channel_name);
-            NotificationManager notificationManager =
-                    getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
-                    channelName, NotificationManager.IMPORTANCE_LOW));
-        }
-
-        if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
-                Object value = getIntent().getExtras().get(key);
-                Log.d(TAG, "Key: " + key + " Value: " + value);
-            }
-        }
+        mFunctions = FirebaseFunctions.getInstance();
     }
 
     @Override
--- app/src/main/java/vmrgamer/firebasecloudmessaging/MyFirebaseMessagingService.java
+++ app/src/main/java/vmrgamer/firebasecloudmessaging/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package vmrgamer.firebasecloudmessaging;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,6 +10,7 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
@@ -19,26 +21,36 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Messages", "Messages", importance);
+            channel.setDescription("All messages.");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // check this for no-call: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        createNotificationChannel();
+
+        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (true) {
-                scheduleJob();
-            } else {
-                handleNow();
-            }
-
+            NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+            Notification notification = new NotificationCompat.Builder(this, "Messages")
+                    .setContentText(remoteMessage.getData().get("text"))
+                    .setContentTitle("New message")
+                    .setSmallIcon(R.drawable.ic_stat_notification)
+                    .build();
+            manager.notify(0, notification);
         }
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        //check sendNotification method for more details
-
     }
 
     @Override
